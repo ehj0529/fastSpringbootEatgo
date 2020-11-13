fe3c7fcc8db0ebaa8df3bcf90cd305c556400b3a
ehj0529 <63188985+ehj0529@users.noreply.github.com>
2020-11-13 22:34:08 +0900

    프로젝트 분리 29강 - 프로젝트 분리 실패

--- eatgo-admin-api/src/main/java/kr/co/fastcampus/eatgo/application/MenuItemService.java
+++ eatgo-admin-api/src/main/java/kr/co/fastcampus/eatgo/application/MenuItemService.java
@@ -4,7 +4,6 @@ import kr.co.fastcampus.eatgo.domain.MenuItem;
 import kr.co.fastcampus.eatgo.domain.MenuItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
