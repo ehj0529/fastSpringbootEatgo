dc106633ea881a3c3077b75ddfb50afca7c82eab
ehj0529 <63188985+ehj0529@users.noreply.github.com>
2020-11-23 23:12:01 +0900

    47강 완료 고객회원 가입시 패스워드 암호화 입력 및 이메일 중복 체크 제외(exception) 처리

--- eatgo-customer-api/src/test/java/kr/co/fastcampus/eatgo/application/UserServiceTests.java
+++ eatgo-customer-api/src/test/java/kr/co/fastcampus/eatgo/application/UserServiceTests.java
@@ -1,13 +1,17 @@
 package kr.co.fastcampus.eatgo.application;
 
+import kr.co.fastcampus.eatgo.domain.User;
 import kr.co.fastcampus.eatgo.domain.UserRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import static org.junit.Assert.*;
+import java.util.Optional;
+
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 public class UserServiceTests {
@@ -35,5 +39,20 @@ public class UserServiceTests {
         userService.registerUser(email, name, password);
 
         verify(userRepository).save(any());
+    }
+
+    @Test(expected = EmailExistedException.class)
+    public void registerUserWithExistedEmail(){
+
+        String email ="tester@example.com";
+        String name ="Tester";
+        String password ="test";
+
+        User user = User.builder().build();
+        given(userRepository.findByEmail(email)).willReturn(Optional.of(user));
+
+        userService.registerUser(email, name, password);
+
+        verify(userRepository, never()).save(any());
     }
 }
